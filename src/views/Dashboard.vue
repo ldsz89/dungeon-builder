4e0c23f608021fa28a07af49495405b9bd38fd21
Dre Shannon <leandre.shannon@centurylink.com>
2020-07-04 09:52:11 -0500

    Created initial naviation to dungeon builder component

--- src/views/Dashboard.vue
+++ src/views/Dashboard.vue
@@ -1,6 +1,24 @@
 <template>
   <div>
     <navigation />
+    <v-card flat>
+      <v-card-title>
+        Welcome to your dungeon dashboard {{ userProfile.name }}!
+      </v-card-title>
+      <v-card-text>
+        Currently, you have no dungeons to display.
+      </v-card-text>
+      <v-card-text>
+        <v-btn
+          width="200"
+          height="100"
+          href
+          to="/dungeon"
+        >
+          <v-icon>mdi-plus</v-icon>
+        </v-btn>
+      </v-card-text>
+    </v-card>
   </div>
 </template>
 
@@ -14,6 +32,9 @@ import Navigation from '@/components/Navigation.vue';
   },
 })
 export default class Dashboard extends Vue {
+  get userProfile() {
+    return this.$store.state.userProfile;
+  }
 }
 </script>
 
