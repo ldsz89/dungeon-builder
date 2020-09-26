9eb33075df0700e8b6bf57ee1476c87ade9c149a
Dre Shannon <leandre.d.shannon@gmail.com>
2020-09-26 10:30:30 -0500

    Creates landmark tiles

---
+++ src/components/tiles/CaravanTile.vue
@@ -0,0 +1,41 @@
+<template>
+  <svg
+    width="50px"
+    height="50px"
+  >
+    <g>
+      <polygon
+        stroke="#000000"
+        stroke-width="0.1"
+        fill="#ffffff"
+        points="12,0 36,0 48,21 36,42 12,42 0,21"
+      />
+      <image
+        href="../../assets/caravan.png"
+        height="36"
+        width="36"
+        x="6"
+        y="2"
+        transform="scale(1)"
+      />
+    </g>
+  </svg>
+</template>
+
+<script lang="ts">
+import {Component, Vue} from 'vue-property-decorator';
+
+@Component
+export default class CaravanTile extends Vue {
+}
+</script>
+
+<style lang="scss" scoped>
+polygon {
+  fill: #9c9c9c;
+}
+
+.active {
+  background: rgb(100, 180, 225);
+}
+</style>
